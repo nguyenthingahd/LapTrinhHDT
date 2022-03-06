06e1c1116274dd68642600c1985a27221180ca3a
nguyenthingahd <100331906+nguyenthingahd@users.noreply.github.com>
2022-03-06 21:11:58 +0700

    submit homework 1

--- bai1.java
+++ bai1.java
@@ -1,29 +1,41 @@
-package homework2;
+package homework1;
 
-//Viết chương trình tính tổng của dãy số Fibonacci đến số Fibonacci <100.
 import java.util.Scanner;
 
 public class bai1 {
-	public static int fibonacci_sum(int n) {
-		if (n < 0 || n >= 100)
-			return 0;
-		int fibo[] = new int[n + 1];
-		fibo[0] = 0;
-		fibo[1] = 1;
-		int sum = fibo[0] + fibo[1];
-		for (int i = 2; i <= n; i++) {
-			fibo[i] = fibo[i - 1] + fibo[i - 2];
-			sum += fibo[i];
-		}
-		return sum;
-	}
 
-	public static void main(String args[]) {
-		Scanner input = new Scanner(System.in);
-		int n;
-		System.out.print("Nhap so phan tu fibonacci muon tinh tong: ");
-		n = input.nextInt();
-		System.out.println("Tong cua day " + n + " so Fibonacci la: " + fibonacci_sum(n));
+	public static void main(String[] args) {
+		int i, j, temp;
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print("Nhập số phần tử của dãy: ");
+			int n = scanner.nextInt();
+
+			int[] arr = new int[n];
+			System.out.print("Nhập các phần tử của dãy: \n");
+			for (i = 0; i < n; i++) {
+				System.out.print("Nhap so thu [" + i + "]: ");
+				arr[i] = scanner.nextInt();
+			}
+			for (i = 0; i < (n - 1); i++) {
+				for (j = 0; j < n - i - 1; j++) {
+					if (arr[j] > arr[j + 1]) {
+						temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+					}
+				}
+			}
+			System.out.println("\nKet qua xap xep tang dan: ");
+			for (i = 0; i < n; i++) {
+				System.out.print(arr[i] + "\t");
+			}
+
+			System.out.println("\nKet qua xap xep giam dan: ");
+			for (i = n - 1; i >= 0; i--) {
+				System.out.print(arr[i] + "\t");
+			}
+		}
+		System.out.println();
 	}
 
 }
--- bai2.java
+++ bai2.java
@@ -1,86 +1,32 @@
-package homework2;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+package homework1;
 
 public class bai2 {
-	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File("C:\\Users\\Admin\\Desktop\\bai2.txt");
-		try (Scanner sc = new Scanner(file)) {
-			int sizeOfRow1 = Integer.parseInt(sc.next());
-			int sizeOfColumn1 = Integer.parseInt(sc.next());
+	int binarySearch(int arr[], int l, int r, int x) {
+		if (r >= l) {
+			int mid = l + (r - l) / 2;
 
-			int[][] matrix1 = new int[sizeOfRow1][sizeOfColumn1];
+			if (arr[mid] == x)
+				return mid;
 
-			for (int row = 0; row < sizeOfRow1; row++) {
-				for (int column = 0; column < sizeOfColumn1; column++) {
+			if (arr[mid] > x)
+				return binarySearch(arr, l, mid - 1, x);
 
-					matrix1[row][column] = Integer.parseInt(sc.next());
-				}
-			}
-
-			sc.nextLine();
-
-			int sizeOfRow2 = Integer.parseInt(sc.next());
-			int sizeOfColumn2 = Integer.parseInt(sc.next());
-
-			int[][] matrix2 = new int[sizeOfRow2][sizeOfColumn2];
-
-			for (int row = 0; row < sizeOfRow2; row++) {
-				for (int column = 0; column < sizeOfColumn2; column++) {
-
-					matrix2[row][column] = Integer.parseInt(sc.next());
-				}
-			}
-			System.out.println("Ma tran 1");
-			for (int row = 0; row < sizeOfRow1; row++) {
-				for (int column = 0; column < sizeOfColumn1; column++) {
-					System.out.print(matrix1[row][column] + "\t");
-				}
-				System.out.print("\n");
-			}
-			System.out.print("\n");
-			System.out.println("Ma tran 2");
-			for (int row = 0; row < sizeOfRow2; row++) {
-				for (int column = 0; column < sizeOfColumn2; column++) {
-					System.out.print(matrix2[row][column] + "\t");
-				}
-				System.out.print("\n");
-			}
-			System.out.print("\n");
-			int[][] matrix3 = new int[sizeOfRow1][sizeOfColumn2];
-			for (int row = 0; row < sizeOfRow1; row++) {
-				for (int column = 0; column < sizeOfColumn2; column++) {
-					matrix3[row][column] = matrix1[row][column] + matrix2[row][column];
-				}
-			}
-			System.out.println("Tong cua ma tran 1 va 2 la: ");
-			for (int row = 0; row < sizeOfRow1; row++) {
-				for (int column = 0; column < sizeOfColumn2; column++) {
-					System.out.print(matrix3[row][column] + "\t");
-				}
-				System.out.print("\n");
-			}
-			System.out.print("\n");
-			int[][] matrix4 = new int[sizeOfRow1][sizeOfColumn2];
-			for (int row = 0; row < sizeOfRow2; row++) {
-				for (int column = 0; column < sizeOfColumn2; column++) {
-					matrix4[row][column] = 0;
-					for (int k = 0; k < sizeOfColumn1; k++) {
-						matrix4[row][column] = matrix4[row][column] + (matrix1[row][k] * matrix2[k][column]);
-					}
-				}
-			}
-			System.out.println("Tich cua ma tran 1 va 2 la: ");
-			for (int row = 0; row < sizeOfRow1; row++) {
-				for (int column = 0; column < sizeOfColumn2; column++) {
-					System.out.print(matrix4[row][column] + "\t");
-				}
-				System.out.print("\n");
-			}
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
+			return binarySearch(arr, mid + 1, r, x);
 		}
+
+		return -1;
 	}
+
+	public static void main(String args[]) {
+		bai2 ob = new bai2();
+		int arr[] = { 2, 4, 9, 10, 11, 22, 24, 31, 48, 56, 76, 86 };
+		int n = arr.length;
+		int x = 10;
+		int result = ob.binarySearch(arr, 0, n - 1, x);
+		if (result == -1)
+			System.out.println("Phần tử không tồn tại.");
+		else
+			System.out.println("Phần tử được tìm thấy tại vị trí: " + result);
+	}
+
 }
